f32fa8b149466e2e054bba6bd3553e6725e86076
Annie Truex <artruex@gmail.com>
2019-10-09 15:08:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+    attr_accessor :name, :grade, :id
+  
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
 
 end
